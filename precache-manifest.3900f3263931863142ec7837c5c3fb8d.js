5bb20e961790f057bae583c7db298eacccf63271
sameerkhan00 <sk2655391@gmail.com>
2020-09-21 18:13:32 +0500

    react

---
+++ precache-manifest.3900f3263931863142ec7837c5c3fb8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc0d5367c68c5fa63c107e703848402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62070b6421cb3d47d213",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2cc147c46c5b3f084992",
+    "url": "/static/css/main.e3065e13.chunk.css"
+  },
+  {
+    "revision": "62070b6421cb3d47d213",
+    "url": "/static/js/2.9c48f9d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9c48f9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc147c46c5b3f084992",
+    "url": "/static/js/main.9f7be2ae.chunk.js"
+  },
+  {
+    "revision": "913c289afb37700f73fc",
+    "url": "/static/js/runtime-main.43af0137.js"
+  }
+]);
